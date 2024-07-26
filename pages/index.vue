1fcf6d0be6db39e92db51d3a4a68e4a5a1a4155f
Erine Moira <erine@archangels.dev>
2024-07-26 21:08:05 +0700

    New component for practice, remove unnecessary codes.

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <v-main>
       <v-container fluid class="pa-0">
         <header-section />
+        <ErineSection />
         <features-section />
         <devices-section />
         <workflow-section />
@@ -13,26 +14,3 @@
     </v-main>
   </v-app>
 </template>
-
-<script>
-import HeaderSection from '../components/HeaderSection.vue'
-import FeaturesSection from '../components/FeaturesSection.vue'
-import DevicesSection from '../components/DevicesSection.vue'
-import WorkflowSection from '../components/WorkflowSection.vue'
-import PartnersSection from '../components/PartnersSection.vue'
-import DownloadSection from '../components/DownloadSection.vue'
-import FooterSection from '../components/FooterSection.vue'
-
-export default {
-  name: 'MainPage',
-  components: {
-    HeaderSection,
-    FeaturesSection,
-    DevicesSection,
-    WorkflowSection,
-    PartnersSection,
-    DownloadSection,
-    FooterSection,
-  },
-}
-</script>
