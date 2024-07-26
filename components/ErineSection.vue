7c741c48da448f86f6987fa4b0aa78461a52191d
Erine Moira <erine@archangels.dev>
2024-07-26 21:21:08 +0700

    Should be an icon here.

--- components/ErineSection.vue
+++ components/ErineSection.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    
+    <h1>Look at this cool SVG!</h1>
+    <img src="../assets/images/icon-twitter.svg" alt="Should be an icon here.">
   </div>
 </template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
-</style>
